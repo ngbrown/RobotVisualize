86efb6ed1f330e00e6a2ece337bd79e8ffc6f2d8
Nathan Brown <nathan@nkbrown.us>
2014-01-01 17:38:01 -0700

    Loading and saving settings.  Loading and showing video.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,23 +1,70 @@
 #include "testApp.h"
+#include "ofGstVideoPlayer.h"
 
 //--------------------------------------------------------------
 void testApp::setup(){
+	video.setPlayer(ofPtr<ofGstVideoPlayer>(new ofGstVideoPlayer));
+	ofBackground(255,255,255);
+	ofSetVerticalSync(true);
+	ofSetFrameRate(60);
 
+	// set parameter names
+	parameters.setName("testApp");
+	parameter_video_path.setName("video_path");
+	parameters.add(parameter_video_path);
+
+	// register listeners
+	gui_button_open_file.addListener(this, &testApp::buttonOpenFilePressed);
+
+	// setup gui.
+	gui_settings.setup(parameters, "settings.xml");
+	gui_settings.add(gui_button_open_file.setup("Open File"));
+	gui_settings.add(gui_label_file_name.setup("File Name", "<No file loaded>"));
+
+	gui_settings.loadFromFile("settings.xml");
+
+	// load video
+	if (ofFile::doesFileExist(parameter_video_path)) {
+		loadVideoFile(parameter_video_path);
+	}
+}
+
+void testApp::exit() {
+	gui_button_open_file.removeListener(this, &testApp::buttonOpenFilePressed);
+	video.close();
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+	if(video.isLoaded()) {
+		video.update();
+	}
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
+	ofBackground(0);
 
+	// draw video
+	if(video.isLoaded()) {
+		video.draw(250, 20);
+	}
+
+	// draw gui
+	gui_settings.draw();
+
+	// draw frame rate
+    ofSetHexColor(0xFFFFFF);
+	ofDrawBitmapString("framerate: " + ofToString(ofGetFrameRate()), ofGetWindowWidth() - 150, 20);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
+	switch (key) {
+		case ' ':
+			video_control_playing = !video_control_playing;
+			video.setPaused(!video_control_playing);
+	}
 }
 
 //--------------------------------------------------------------
@@ -58,4 +105,33 @@ void testApp::gotMessage(ofMessage msg){
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){ 
 
+}
+
+void testApp::buttonOpenFilePressed() {
+	ofLogNotice("testApp") << "Open File started";
+	ofFileDialogResult result = ofSystemLoadDialog("Select Movie File", false, parameter_video_path);
+
+	if (result.bSuccess == false) {
+		ofLogWarning("testApp") << "Open File failed";
+		gui_label_file_name = "<No file loaded>";
+	} else {
+		parameter_video_path = result.getPath();
+
+		loadVideoFile(result.getPath());
+	}
+}
+
+void testApp::loadVideoFile(std::string file_path) {
+	ofLogNotice("testApp") << "Loading video \"" << file_path << "\"";
+
+	gui_label_file_name = ofFilePath::getFileName(file_path);
+
+	video.loadMovie(file_path);
+
+	if(video.isLoaded()) {
+		ofLogNotice("testApp") << "Video loaded";
+	} else {
+		ofLogWarning("testApp") << "Video loading failed";
+		gui_label_file_name = "<No file loaded>";
+	}
 }
--- src/testApp.h
+++ src/testApp.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxGui.h"
 
 class testApp : public ofBaseApp{
 
 	public:
+		testApp() 
+			: video_control_playing(false)
+		{ }
+
 		void setup();
+		void exit();
 		void update();
 		void draw();
 
@@ -18,5 +25,23 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
+	
+	private:
+		ofxCv::FlowFarneback farneback;
+		ofxCv::FlowPyrLK pyrLk;
+	
+		ofxCv::Flow* curFlow;
+		ofVideoPlayer video;
+
+		bool video_control_playing;
+
+		ofxPanel gui_settings;
+		ofxButton gui_button_open_file;
+		ofxLabel gui_label_file_name;
+
+		ofParameterGroup parameters;
+		ofParameter<std::string> parameter_video_path;
+
+		void loadVideoFile(std::string file_path);
+		void buttonOpenFilePressed();
 };
