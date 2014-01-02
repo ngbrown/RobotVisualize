3aa1c9a790ef24b040558b5f4ec4b51556d2164a
Nathan Brown <nathan@nkbrown.us>
2014-01-01 21:57:31 -0700

    Switch between Farneback and Pyramid Lucas-Kanade.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -8,18 +8,30 @@ void testApp::setup(){
 	ofSetVerticalSync(true);
 	ofSetFrameRate(60);
 
-	// set parameter names
+	// use ofParameterGroup so we can set parameters not directly exposed on GUI
 	parameters.setName("testApp");
-	parameter_video_path.setName("video_path");
-	parameters.add(parameter_video_path);
+	parameters.add(parameter_video_path.set("video_path", ""));
 
 	// register listeners
 	gui_button_open_file.addListener(this, &testApp::buttonOpenFilePressed);
+	gui_button_reset_features.addListener(this, &testApp::buttonResetFeaturesPressed);
+	parameter_use_farneback.addListener(this, &testApp::toggleButtonFarneback);
+	parameter_use_pyrlk.addListener(this, &testApp::toggleButtonPyrlk);
 
-	// setup gui.
+		// setup gui.
 	gui_settings.setup(parameters, "settings.xml");
 	gui_settings.add(gui_button_open_file.setup("Open File"));
 	gui_settings.add(gui_label_file_name.setup("File Name", "<No file loaded>"));
+
+	gui_settings.add(gui_settings_farneback.setup("Farneback"));
+	gui_settings_farneback.add(parameter_farneback_pyramid_scale.set("pyramid_scale", 0.5, 0, 1));
+	gui_settings_farneback.add(parameter_farneback_num_levels.set("num_levels", 4, 1, 8));
+	gui_settings.add(parameter_use_farneback.set("use_farneback", true));
+
+	gui_settings.add(gui_settings_pyrlk.setup("Pyramid Lucas-Kanade"));
+	gui_settings_pyrlk.add(gui_button_reset_features.setup("Reset Features"));
+	gui_settings.add(parameter_use_pyrlk.set("use_pyrlk", false));
+
 
 	gui_settings.loadFromFile("settings.xml");
 
@@ -27,6 +39,8 @@ void testApp::setup(){
 	if (ofFile::doesFileExist(parameter_video_path)) {
 		loadVideoFile(parameter_video_path);
 	}
+
+	curFlow = &farneback;
 }
 
 void testApp::exit() {
@@ -38,6 +52,18 @@ void testApp::exit() {
 void testApp::update(){
 	if(video.isLoaded()) {
 		video.update();
+
+		if (video.isFrameNew()) {
+			if (parameter_use_farneback) {
+				curFlow = &farneback;
+				farneback.setPyramidScale(parameter_farneback_pyramid_scale);
+				farneback.setNumLevels(parameter_farneback_num_levels);
+			} else if (parameter_use_pyrlk) {
+				curFlow = &pyrLk;
+			}
+
+			curFlow->calcOpticalFlow(video);
+		}
 	}
 }
 
@@ -47,7 +73,9 @@ void testApp::draw(){
 
 	// draw video
 	if(video.isLoaded()) {
-		video.draw(250, 20);
+		ofRectangle video_rectangle(250, 20, 320, 240);
+		video.draw(video_rectangle);
+		curFlow->draw(video_rectangle);
 	}
 
 	// draw gui
@@ -64,6 +92,10 @@ void testApp::keyPressed(int key){
 		case ' ':
 			video_control_playing = !video_control_playing;
 			video.setPaused(!video_control_playing);
+			break;
+        case OF_KEY_RIGHT:
+            video.nextFrame();
+			break;
 	}
 }
 
@@ -118,6 +150,34 @@ void testApp::buttonOpenFilePressed() {
 		parameter_video_path = result.getPath();
 
 		loadVideoFile(result.getPath());
+	}
+}
+
+void testApp::buttonResetFeaturesPressed() {
+	pyrLk.resetFeaturesToTrack();
+}
+
+void testApp::toggleButtonFarneback(bool& value) {
+	if (value == true) {
+		parameter_use_pyrlk = !value;
+		fixupMethodGroups();
+	}
+}
+
+void testApp::toggleButtonPyrlk(bool& value) {
+	if (value == true) {
+		parameter_use_farneback = !value;
+		fixupMethodGroups();
+	}
+}
+
+void testApp::fixupMethodGroups() {
+	if (parameter_use_farneback) {
+		gui_settings_farneback.maximize();
+		gui_settings_pyrlk.minimize();
+	} else if (parameter_use_pyrlk) {
+		gui_settings_pyrlk.maximize();
+		gui_settings_farneback.minimize();
 	}
 }
 
--- src/testApp.h
+++ src/testApp.h
@@ -7,8 +7,8 @@
 class testApp : public ofBaseApp{
 
 	public:
-		testApp() 
-			: video_control_playing(false)
+		testApp() : 
+			video_control_playing(false)
 		{ }
 
 		void setup();
@@ -42,6 +42,23 @@ class testApp : public ofBaseApp{
 		ofParameterGroup parameters;
 		ofParameter<std::string> parameter_video_path;
 
+		ofParameter<bool> parameter_use_pyrlk;
+		ofParameter<bool> parameter_use_farneback;
+
+		ofxPanel gui_settings_farneback;
+		ofParameter<float> parameter_farneback_pyramid_scale;
+		ofParameter<int> parameter_farneback_num_levels;
+
+		ofxPanel gui_settings_pyrlk;
+		ofxButton gui_button_reset_features;
+
 		void loadVideoFile(std::string file_path);
+
 		void buttonOpenFilePressed();
+		void buttonResetFeaturesPressed();
+
+		void toggleButtonFarneback(bool& value);
+		void toggleButtonPyrlk(bool& value);
+
+		void fixupMethodGroups();
 };
